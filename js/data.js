37006b853d12e9e4500cd59b617f9e9dc73fc0e4
Lara Aguerre <aguerre.lara@gmail.com>
2024-02-01 16:39:18 +0100

    position adjustment

--- js/data.js
+++ js/data.js
@@ -20,11 +20,11 @@ const invadersData = [
 
     { imgSource: './img/faces/ken.png', top: 150, left: 640, topVel: 0.2, leftVel: 1.5 },
 
-    { imgSource: './img/faces/lara.png', top: 210, left: 235, topVel: 0.2, leftVel: 1.5 },
+    { imgSource: './img/faces/lara.png', top: 220, left: 235, topVel: 0.2, leftVel: 1.5 },
 
-    { imgSource: './img/faces/alejandro.png', top: 210, left: 370, topVel: 0.2, leftVel: 1.5 },
+    { imgSource: './img/faces/alejandro.png', top: 220, left: 370, topVel: 0.2, leftVel: 1.5 },
 
-    { imgSource: './img/faces/german.png', top: 210, left: 505, topVel: 0.2, leftVel: 1.5 },
+    { imgSource: './img/faces/german.png', top: 220, left: 505, topVel: 0.2, leftVel: 1.5 },
 
 ]
 
@@ -50,10 +50,10 @@ const newInvadersData = [
 
     { imgSource: './img/faces/maria.png', top: 150, left: 640, topVel: 0.3, leftVel: 2.5 },
 
-    { imgSource: './img/faces/german.png', top: 210, left: 235, topVel: 0.3, leftVel: 2.5 },
+    { imgSource: './img/faces/german.png', top: 220, left: 235, topVel: 0.3, leftVel: 2.5 },
 
-    { imgSource: './img/faces/fran.png', top: 210, left: 370, topVel: 0.3, leftVel: 2.5 },
+    { imgSource: './img/faces/fran.png', top: 220, left: 370, topVel: 0.3, leftVel: 2.5 },
 
-    { imgSource: './img/faces/tere.png', top: 210, left: 505, topVel: 0.3, leftVel: 2.5 },
+    { imgSource: './img/faces/tere.png', top: 220, left: 505, topVel: 0.3, leftVel: 2.5 },
 
 ]
